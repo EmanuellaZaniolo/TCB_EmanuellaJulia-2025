f52913ea86700060ee041c9b101e9270a7ba597d
emanuellaZaniolo <zaniolo.emanuella@gmail.com>
2025-10-31 11:15:31 -0300

    LP

---
+++ file-io-project/src/main/java/br/edu/ifpr/tcb_bio/modelo/Contato.java
@@ -0,0 +1,43 @@
+package br.edu.ifpr.tcb_bio.modelo;
+
+public class Contato {
+    private int id;
+    private String nome;
+    private String celular;
+    private String email;
+    private Endereco endereco;
+    public Contato(){
+
+    }
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCelular() {
+        return celular;
+    }
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
---
+++ file-io-project/src/main/java/br/edu/ifpr/tcb_bio/view/Main.java
@@ -0,0 +1,7 @@
+package br.edu.ifpr.tcb_bio.view;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
